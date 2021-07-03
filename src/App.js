5155d6a7fd351da46f697ac7278b725f0b6188c1
adrian3332 <74941032+adrian3332@users.noreply.github.com>
2021-07-03 18:36:26 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/navbar/navbar';
+import ListCards from './components/cards/ListCards';
+function App() {
+  return (
+    <div className="App">
+      <h1>Mi primera aplicacion en react</h1>
+      <Navbar/>
+      <ListCards/>
+    </div>
+  );
+}
+
+export default App;
+ 
---
+++ src/components/cards/ListCards.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Cards from './Cards'
+
+const ListCards = () => {
+    return (
+        <div>
+            <h1>Listado de Cards</h1>
+            <Cards/>
+        </div>
+    )
+}
+
+export default ListCards
